776f29c8b1ec62bf80c8f04f31e8606546524d4d
Uladzislau Flaryanovich <vlfl2612@yandex.by>
2021-11-10 22:44:26 +0300

    Initial commit

---
+++ app/src/main/java/by/vlfl/campos/presentation/view/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package by.vlfl.campos.presentation.view.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.vlfl.campos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
